1fe0bc1e31605af9b80c5a0896c04debd0deecdb
Steve Corwin <37982371+PrinceCorwin@users.noreply.github.com>
2021-10-31 14:55:27 -0400

    Initial file upload

---
+++ src/ItemList.js
@@ -0,0 +1,17 @@
+import LineItem from './LineItem';
+const ItemList = ({ items, handleDelete, handleCheck }) => {
+  return (
+    <ul>
+      {items.map((item) => (
+        <LineItem
+          key={item.id}
+          item={item}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default ItemList;
---
+++ src/LineItem.js
@@ -0,0 +1,30 @@
+import { FaTrashAlt } from 'react-icons/fa';
+
+const LineItem = ({ item, handleDelete, handleCheck }) => {
+  return (
+    <li className='item' key={item.id}>
+      <input
+        type='checkbox'
+        // the following onChange call must use anonymous function format so we can pass an argument without the handleCheck function executing immediately. onChange={handleCheck(item.id)} would fire off immediately without user interaction
+        onChange={() => handleCheck(item.id)}
+        checked={item.checked}
+      />
+      <label
+        onDoubleClick={() => handleCheck(item.id)}
+        style={
+          item.checked ? { textDecoration: 'line-through', color: 'red' } : null
+        }
+      >
+        {item.item}
+      </label>
+      <FaTrashAlt
+        role='button'
+        tabIndex='0'
+        onClick={() => handleDelete(item.id)}
+        aria-label={`Delete ${item.item}`}
+      />
+    </li>
+  );
+};
+
+export default LineItem;
