851489720cc0a2be7b94988011b3f0b98ebe73b8
Steve Corwin <37982371+PrinceCorwin@users.noreply.github.com>
2021-11-02 20:49:41 -0400

    Fixed ghost entry bug / added useEffect

--- src/App.js
+++ src/App.js
@@ -3,37 +3,41 @@ import AddItem from './AddItem';
 import SearchItem from './SearchItem';
 import Content from './Content';
 import Footer from './Footer';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 function App() {
   const [items, setItems] = useState(
-    JSON.parse(localStorage.getItem('shoppinglist'))
+    JSON.parse(localStorage.getItem('shoppinglist') || [])
   );
 
   const [newItem, setNewItem] = useState('');
   const [search, setSearch] = useState('');
 
-  const setAndSave = (newItems) => {
-    setItems(newItems);
-    localStorage.setItem('shoppinglist', JSON.stringify(newItems));
-  };
+  useEffect(() => {
+    localStorage.setItem('shoppinglist', JSON.stringify(items));
+  }, [items]);
+
+  // const setAndSave = (newItems) => {
+  //   setItems(newItems);
+  //   localStorage.setItem('shoppinglist', JSON.stringify(newItems));
+  // };
 
   const addItem = (item) => {
     const id = items.length ? items.length + 1 : 1;
     const myNewItem = { id, checked: false, item };
     const listItems = [...items, myNewItem];
-    setAndSave(listItems);
+    setItems(listItems);
   };
 
   const handleCheck = (id) => {
     const listItems = items.map((item) =>
       item.id === id ? { ...item, checked: !item.checked } : item
     );
-    setAndSave(listItems);
+    setItems(listItems);
   };
 
   const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id);
-    setAndSave(listItems);
+    setItems(listItems);
   };
 
   const handleSubmit = (e) => {
